2e06f6cebcebfe09cb4183e8bba44338d7417148
Codewith-soham <ghadgesoham2006@gmail.com>
2026-09-16 21:51:23 +0530

    feat: add deleteTodo route to todoRoutes

--- src/routes/todo.route.ts
+++ src/routes/todo.route.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from "fastify";
-import { createTodo, getAllTodos, getTodoById } from  "../controller/todo.controller.js";
+import { createTodo, deleteTodo, getAllTodos, getTodoById } from  "../controller/todo.controller.js";
 
 export const todoRoutes = async(app: FastifyInstance) => {
     app.post("/createTodo", createTodo)
     app.get("/todos", getAllTodos)
     app.get("/todos/:id", getTodoById)
+    app.delete("/deleteTodo/:id", deleteTodo)
 }
